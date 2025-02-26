2b2bfbf9260dc226e2f8c7f439e8d660c7f4d7e1
vibhuti-jadav <vibhutij109@gmail.com>
2025-02-26 15:34:34 +0530

    adding

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Custom Cursor
+const cursor = document.querySelector('.custom-cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+});
+
+document.addEventListener('mousedown', () => {
+    cursor.style.transform = 'scale(0.8)';
+});
+
+document.addEventListener('mouseup', () => {
+    cursor.style.transform = 'scale(1)';
+});
